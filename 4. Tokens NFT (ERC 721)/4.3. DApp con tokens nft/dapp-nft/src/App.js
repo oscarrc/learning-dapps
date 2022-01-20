fcbf91f41b5d5992b7461cda3bc7ea3920fb9e11
Oscar <oscarrc.web@gmail.com>
2022-01-20 13:51:14 +0100

    [MOD] App template

--- 4. Tokens NFT (ERC 721)/4.3. DApp con tokens nft/dapp-nft/src/App.js
+++ 4. Tokens NFT (ERC 721)/4.3. DApp con tokens nft/dapp-nft/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow px-4">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="https://frogames.es/rutas-de-aprendizaje"
@@ -16,7 +16,7 @@ function App() {
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 d-flex text-center justify-content-center">
               <div className="content mr-auto ml-auto">
                 <a
                   href="https://frogames.es/rutas-de-aprendizaje"
