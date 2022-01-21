84afdfd1128a59eaac9d4d9022f5b719049c9824
Oscar <oscarrc.web@gmail.com>
2022-01-21 18:25:33 +0100

    [MOD] Unused imoprt

--- 4. Tokens NFT (ERC 721)/4.3. DApp con tokens nft/dapp-nft/src/truffle/contracts/Color.sol
+++ 4. Tokens NFT (ERC 721)/4.3. DApp con tokens nft/dapp-nft/src/truffle/contracts/Color.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.4.22 <0.9.0;
 
-import "../../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../../../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract Color is ERC721Enumerable {
