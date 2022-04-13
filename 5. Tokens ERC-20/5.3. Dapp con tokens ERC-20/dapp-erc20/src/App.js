89ef65b5d17dd588bbc3915edff3722e7c440ec1
Oscar <oscarrc.web@gmail.com>
2022-04-13 14:19:56 +0200

    [MOD] Styling

--- 5. Tokens ERC-20/5.3. Dapp con tokens ERC-20/dapp-erc20/src/App.js
+++ 5. Tokens ERC-20/5.3. Dapp con tokens ERC-20/dapp-erc20/src/App.js
@@ -99,78 +99,78 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow px-4">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            DApp ERC-20
-          </a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-block">
-              <span className="badge bg-secondary">{account}</span>
-            </li>
-          </ul>
-        </nav>
-        <div className="container-fluid my-5">
-          <main className="row d-flex flex-column justify-content-center align-items-center">
-            <section className="col-12 col-md-8 col-lg-4 content text-center my-4">
-              <h1>Compra de tokens ERC-20</h1>
-              <form onSubmit={ (e) => {
-                e.preventDefault();
-                buyTokens(address, amount);
-                setContractBalance(null);
+    <div className="d-flex flex-column min-vh-100">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow px-4">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DApp ERC-20
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-block">
+            <span className="badge bg-secondary">{account}</span>
+          </li>
+        </ul>
+      </nav>
+      <div className="container-fluid my-5 flex-grow-1">
+        <main className="row d-flex flex-column justify-content-center align-items-center">
+          <section className="col-12 col-md-8 col-lg-4 content text-center my-4">
+            <h1>Compra de tokens ERC-20</h1>
+            <form onSubmit={ (e) => {
+              e.preventDefault();
+              buyTokens(address, amount);
+              setContractBalance(null);
+              setBalance(null);
+            }}>
+              <input className="form-control mb-1" type="text" placeholder="Dirección del destinatario" value={address} onChange={ (e) => setAddress(e.target.value) } />
+              <input className="form-control mb-1"  type="text" placeholder="Cantidad de tokens" value={amount} onChange={ (e) => setAmount(e.target.value) } />
+              <button className="btn w-100 btn-danger btn-sm" type="submit">Comprar Tokens</button>
+            </form>
+          </section>
+          <section className="col-12 col-md-8 col-lg-4 content text-center my-4">
+            <h1>Consulta de balance</h1>
+            <form onSubmit={ (e) => {
+              e.preventDefault();
+              getBalance(address);
+            }}>
+              <input className="form-control mb-1" type="text" placeholder="Dirección a consultar" value={address} onChange={ (e) => {
+                setAddress(e.target.value)
                 setBalance(null);
-              }}>
-                <input className="form-control mb-1" type="text" placeholder="Dirección del destinatario" value={address} onChange={ (e) => setAddress(e.target.value) } />
-                <input className="form-control mb-1"  type="text" placeholder="Cantidad de tokens" value={amount} onChange={ (e) => setAmount(e.target.value) } />
-                <button className="btn w-100 btn-danger btn-sm" type="submit">Comprar Tokens</button>
-              </form>
-            </section>
-            <section className="col-12 col-md-8 col-lg-4 content text-center my-4">
-              <h1>Consulta de balance</h1>
-              <form onSubmit={ (e) => {
-                e.preventDefault();
-                getBalance(address);
-              }}>
-                <input className="form-control mb-1" type="text" placeholder="Dirección a consultar" value={address} onChange={ (e) => {
-                  setAddress(e.target.value)
-                  setBalance(null);
-                } } />
-                {
-                  balance ?
-                    <div className="alert alert-primary mb-1" role="alert">
-                      { balance } tokens
-                    </div> :
-                    null
-                }
-                <button className="btn w-100 btn-primary btn-sm" type="submit">Ver balance</button>
-              </form>
-            </section>
-            <section className="col-12 col-md-8 col-lg-4 content text-center my-4">
-              <h1>Tokens disponibles</h1>
-              <form onSubmit={ (e) => {
-                e.preventDefault();
-                getSupply();
-              }}>
-                {
-                  contractBalance ?
-                    <div className="alert alert-success mb-1" role="alert">
-                      { contractBalance } tokens disponibles
-                    </div> :
-                    null
-                }
-                <button className="btn w-100 btn-success btn-sm" type="submit">Consultar tokens disponibles</button>
-              </form>
-            </section>
-          </main>
-        </div>
-        <footer className="d-flex justify-content-center text-center">
-          <span className="badge bg-dark">Contract: {contractAddress}</span>
-        </footer>
+              } } />
+              {
+                balance ?
+                  <div className="alert alert-primary mb-1" role="alert">
+                    { balance } tokens
+                  </div> :
+                  null
+              }
+              <button className="btn w-100 btn-primary btn-sm" type="submit">Ver balance</button>
+            </form>
+          </section>
+          <section className="col-12 col-md-8 col-lg-4 content text-center my-4">
+            <h1>Tokens disponibles</h1>
+            <form onSubmit={ (e) => {
+              e.preventDefault();
+              getSupply();
+            }}>
+              {
+                contractBalance ?
+                  <div className="alert alert-success mb-1" role="alert">
+                    { contractBalance } tokens disponibles
+                  </div> :
+                  null
+              }
+              <button className="btn w-100 btn-success btn-sm" type="submit">Consultar tokens disponibles</button>
+            </form>
+          </section>
+        </main>
+      </div>
+      <footer className="d-flex justify-content-center text-center py-4" >
+        <span className="badge bg-dark">Contract: {contractAddress}</span>
+      </footer>
     </div>
   );
 }
