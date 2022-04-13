b37872e16f09ddca7fc97f0e8b76280e12d6a046
Oscar <oscarrc.web@gmail.com>
2022-04-13 11:18:04 +0200

    [ADD] dapp erc20 template

---
+++ 5. Tokens ERC-20/5.3. Dapp con tokens ERC-20/dapp-erc20/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow px-4">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            DApp Lotería
+          </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-block">
+              <small className="text-white"></small>
+            </li>
+          </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default App;
